64ae94dd660e30d09e0d7898f69834e6b8b4214a
Paul <test@mail.ru>
2018-01-30 23:07:26 +0400

    category_view

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from django.conf.urls import url
-from landing import views
+from categories import views
 
 urlpatterns = [
     # url(r'^', views.landing, name='landing')
+    url(r'^category/(?P<category_id>\w+)/$', views.category, name='category')
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
+from categories.models import *
+from products.models import *
 
+def category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    products = Product.objects.filter(is_active=True)
+    products_images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    #print(request.session.session_key)
+    return render(request, 'categories/category.html', locals())
